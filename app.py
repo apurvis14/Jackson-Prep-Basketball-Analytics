d3df97d71d46b6e93dc0c823eaf4620961d3efea
apurvis14 <purvis12@purdue.edu>
2025-09-15 14:52:22 -0500

    photo adjustment2

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ with tab1:
 
     with left_col:
         if selected_player == "Team":
-            st.image("photos/team_logo.png", width=150)
+            st.image("photos/team_logo.png", width=175)
         else:
             st.image(f"photos/{selected_player}.JPG", width=200)
 
