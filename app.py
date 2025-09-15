ae70d25e78b10c8e24366d8b102220ffefc18bdc
apurvis14 <purvis12@purdue.edu>
2025-09-15 12:48:22 -0500

    Image Width2

--- app.py
+++ app.py
@@ -356,7 +356,7 @@ with left_col:
     if selected_player == "Team":
         st.image("photos/team_logo.JPG", width=200)
     else:
-        st.image(f"photos/{selected_player}.JPG", width=250)
+        st.image(f"photos/{selected_player}.JPG", width=200)
 
 with right_col:
     if selected_player == "Team":
