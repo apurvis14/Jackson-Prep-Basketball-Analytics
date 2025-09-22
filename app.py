3c60e4b82bcee93b8ef29be2a6ddbf1d55e84da0
apurvis14 <purvis12@purdue.edu>
2025-09-22 13:57:15 -0500

    New Look

--- app.py
+++ app.py
@@ -212,4 +212,4 @@ with tab3:
             cell.set_edgecolor('black')
             cell.get_text().set_fontweight('bold')
 
-    st.dataframe(hustle)
+    st.dataframe(hustle, use_container_width=True, height=800)
