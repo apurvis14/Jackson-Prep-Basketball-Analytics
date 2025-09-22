707576a0719194d5ea1de1854d95b07cdf923d3f
apurvis14 <purvis12@purdue.edu>
2025-09-22 15:24:01 -0500

    Size Adjustments

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ with tab3:
 
     # Resize to ensure fit and readability
     table.auto_set_font_size(False)
-    table.set_fontsize(24)
+    table.set_fontsize(26)
     table.scale(1.4, 2.5)
 
     # Change Color the header row
