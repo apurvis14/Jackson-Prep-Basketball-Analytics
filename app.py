34bbf4230e8c6c265f6d3ae3e36848e8a467f1e2
apurvis14 <purvis12@purdue.edu>
2025-09-22 14:00:43 -0500

    Increase Height

--- app.py
+++ app.py
@@ -212,4 +212,4 @@ with tab3:
             cell.set_edgecolor('black')
             cell.get_text().set_fontweight('bold')
 
-    st.dataframe(hustle.reset_index(drop=True), use_container_width=True, height=700)
+    st.dataframe(hustle.reset_index(drop=True), use_container_width=True, height=750)
