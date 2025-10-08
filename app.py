d4eadb07bffda3b2c6c991642c53cd4830353626
apurvis14 <purvis12@purdue.edu>
2025-10-08 18:23:15 -0500

    Remove Top Margin

--- app.py
+++ app.py
@@ -181,13 +181,13 @@ with tab1:
             # --- Midrange ---
             makesM, attM, pctM = calc_zone_stats(filtered, "Midrange")
             col2.markdown(styled_text("Midrange", size=22, margin="0px 0px 0px 0px", underline=True, center=True), unsafe_allow_html=True)
-            col2.markdown(styled_text(f"{makesM}/{attM}", size=18, weight="normal", margin="8px 0px 16px 0px",underline=False, center=True), unsafe_allow_html=True)
+            col2.markdown(styled_text(f"{makesM}/{attM}", size=18, weight="normal", margin="0px 0px 16px 0px",underline=False, center=True), unsafe_allow_html=True)
             col2.markdown(styled_text(f"{pctM:.1f}%", size=18, weight="normal", margin="-10px",underline=False, center=True), unsafe_allow_html=True)
 
             # --- 3PT ---
             makes3, att3, pct3 = calc_zone_stats(filtered, "3PT")
             col3.markdown(styled_text("3PT", size=22, margin="0px 0px 4px 0px", underline=True, center=True), unsafe_allow_html=True)
-            col3.markdown(styled_text(f"{makes3}/{att3}", size=18, weight="normal", margin="8px 0px 16px 0px",underline=False, center=True), unsafe_allow_html=True)
+            col3.markdown(styled_text(f"{makes3}/{att3}", size=18, weight="normal", margin="0px 0px 16px 0px",underline=False, center=True), unsafe_allow_html=True)
             col3.markdown(styled_text(f"{pct3:.1f}%", size=18, weight="normal", margin="-10px",underline=False, center=True), unsafe_allow_html=True)
 
         # Shot chart
