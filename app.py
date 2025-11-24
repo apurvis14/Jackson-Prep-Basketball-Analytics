4b93872bb068eaaecdd64dd8dc4c9b49f7e10f33
apurvis14 <purvis12@purdue.edu>
2025-11-23 22:06:17 -0600

    try this

--- app.py
+++ app.py
@@ -1112,7 +1112,7 @@ with tab8:
         press = press[cols]
 
         # Create display version (without Practice Score)
-        press_display = press.drop(columns=['No Advantage','Turnover','Jailbreak','BS Miss','BS Make','ES Make','ES Miss','Fouls','Deflections','Total']).copy()
+        press_display = press.drop(columns=['No Advantage','Turnover','Jailbreak','BS Miss','BS Make','ES Make','ES Miss','Fouls','Deflections']).copy()
 
         fig, ax = plt.subplots(figsize=(32, 36))
         ax.axis('off')
