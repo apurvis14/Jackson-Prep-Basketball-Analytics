085fc2f131d0a232f20acf7d1b44abb6593afe99
apurvis14 <purvis12@purdue.edu>
2025-12-02 09:21:47 -0600

    add second table

--- app.py
+++ app.py
@@ -1154,3 +1154,87 @@ with tab8:
                 cell.set_facecolor("#BDBDBDB0" if row % 2 == 0 else 'white')
 
         st.pyplot(fig)
+
+    if press_df.empty:
+            st.markdown(
+            styled_text(
+                f"No Press Effectiveness Stats Available for {selected_game}",
+                size=32,
+                weight='bold',
+                margin="200px 0px",
+                underline=False,
+                center=True
+            ),
+            unsafe_allow_html=True
+        )
+        
+    else:
+        press_2 = press_df.groupby('Press').agg(
+            {
+                'No Advantage': 'sum',
+                'Turnover': 'sum',
+                'Jailbreak': 'sum',
+                'BS Miss': 'sum',
+                'BS Make': 'sum',
+                'ES Make': 'sum',
+                'ES Miss': 'sum',
+                'Fouls': 'sum',
+                'Deflections': 'sum',
+                'Total': 'sum'
+            }
+        ).reset_index()
+
+        # Fill all NaN with 0
+        press_2 = press_2.fillna(0)
+
+
+        cols = press_2.columns.tolist()
+        press_2 = press_2[cols]
+
+        press_2['No Advantage %'] = round(press_2['No Advantage'] / press_2['Total'] * 100, 2).fillna(0)
+        press_2['Turnover %'] = round(press_2['Turnover'] / press_2['Total'] * 100, 2).fillna(0)
+        press_2['Jailbreak %'] = round(press_2['Jailbreak'] / press_2['Total'] * 100, 2).fillna(0)
+        press_2['BS Miss %'] = round(press_2['BS Miss'] / press_2['Total'] * 100, 2).fillna(0)
+        press_2['BS Make %'] = round(press_2['BS Make'] / press_2['Total'] * 100, 2).fillna(0)
+        press_2['ES Make %'] = round(press_2['ES Make'] / press_2['Total'] * 100, 2).fillna(0)
+        press_2['ES Miss %'] = round(press_2['ES Miss'] / press_2['Total'] * 100, 2).fillna(0)
+        press_2['Fouls %'] = round(press_2['Fouls'] / press_2['Total'] * 100, 2).fillna(0)
+        press_2['Deflections %'] = round(press_2['Deflections'] / press_2['Total'] * 100, 2).fillna(0)
+
+        # Sort Practice DataFrame
+        press_2 = press_2.sort_values(by=['Total'], ascending=False)
+
+        # Create display version (without Practice Score)
+        press_display_2 = press_2.drop(columns=['No Advantage','Turnover','Jailbreak','BS Miss','BS Make','ES Make','ES Miss','Fouls','Deflections']).copy()
+
+        fig, ax = plt.subplots(figsize=(32, 36))
+        ax.axis('off')
+
+        # Create table
+        table = plt.table(
+            cellText=press_display_2.values,
+            colLabels=press_display_2.columns,
+            cellLoc='center',
+            bbox=[-0.05, 0.13, 1.05, 0.95]
+        )
+
+        # Style table
+        table.auto_set_font_size(False)
+        table.set_fontsize(28)
+        table.scale(1.4, 2.5)
+
+        for key, cell in table.get_celld().items():
+            row, col = key
+            if row == 0:
+                cell.set_facecolor('#da1a32')
+                cell.set_linewidth(2.5)
+                cell.set_edgecolor('#0033A0')
+                cell.get_text().set_fontweight('bold')
+                cell.get_text().set_color('#0033A0')
+                cell.set_fontsize(30)
+            else:
+                cell.get_text().set_color('#0033A0')
+                cell.set_edgecolor('#0033A0')
+                cell.set_facecolor("#BDBDBDB0" if row % 2 == 0 else 'white')
+
+        st.pyplot(fig)
