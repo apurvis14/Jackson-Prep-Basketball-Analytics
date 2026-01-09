5ec15adbc5abf44839a2a3f015dcda0e32612f5b
apurvis14 <purvis12@purdue.edu>
2026-01-09 13:00:49 -0600

    try this

--- app.py
+++ app.py
@@ -361,6 +361,24 @@ selected_player_info = (lambda x: f"#{player_info[x]['number']} — {player_info
 if selected_game != "Season":
     game_df = game_df[game_df["GAME"] == str(selected_game)]
 
+    if game_df.empty:
+        game_total_assists = 0
+        game_total_turnovers = 0
+        game_total_off_rebs = 0
+        game_total_def_rebs = 0
+        game_ast_to_ratio = 0
+
+    else:
+        game_total_assists = game_df["Ast"].sum()
+        game_total_turnovers = game_df["TO"].sum()
+        game_total_off_rebs = game_df["OFF_Reb"].sum()
+        game_total_def_rebs = game_df["DEF_Reb"].sum()
+        game_ast_to_ratio = (
+            round(game_total_assists / game_total_turnovers, 2)
+            if game_total_turnovers != 0
+            else game_total_assists
+        )
+
     if selected_player != "Team":
         player_df = game_df[game_df["Player"] == selected_player]
 
